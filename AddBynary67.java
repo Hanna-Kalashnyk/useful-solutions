b7ef01e3476995e22b04d4ff0461fbd494d3fd62
hannakalashnyk <hanna.kalashnik@gmail.com>
2023-10-06 11:36:20 -0700

    8 October 2023

--- AddBynary67.java
+++ AddBynary67.java
@@ -62,4 +62,23 @@ public class AddBynary67
 		sb.append(reserve);
 		return sb.reverse().toString();
 	}
+
+
+	public String addBinary2(String a, String b) {
+		StringBuilder res = new StringBuilder();
+		int i = a.length() - 1;
+		int j = b.length() - 1;
+		int carry = 0;
+		while ( i >=0 || j >= 0 || carry != 0){
+			int achar = i >= 0 ? a.charAt(i) - '0' : 0;
+			int bchar = j >= 0 ? b.charAt(j) - '0' : 0;
+			int sum = achar + bchar + carry;
+
+			res.insert(0, sum%2);
+			carry = sum / 2;
+			i--;
+			j--;
+		}
+		return res.toString();
+	}
 }
---
+++ FindMedianaFromStream295.java
@@ -0,0 +1,26 @@
+//The median is the middle value in an ordered integer list.
+//If the size of the list is even, there is no middle value, and the median is the mean of the two middle values.
+
+        package leetcode;
+
+public class FindMedianaFromStream295 {
+Prio
+    public MedianFinder() {
+
+    }
+
+    public void addNum(int num) {
+
+    }
+
+    public double findMedian() {
+
+    }
+}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
